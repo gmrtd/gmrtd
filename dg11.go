d460ca36f207a839b46cc96da732af542a9e7689
Oscar Sanderson <o.sanderson@gmail.com>
2024-05-01 15:05:07 +0800

    Added DG11 parsing

--- dg11.go
+++ dg11.go
@@ -1,14 +1,41 @@
 package gmrtd
 
 import (
+	"bytes"
 	"fmt"
+	"log"
+	"log/slog"
 	"slices"
+	"strings"
 )
 
 const DG11Tag = 0x6B
 
+// TODO - review parsing (< to space) and < separator on fields
+//			- currently we replace '<' with space, unless spec indicates that '<' is used as separator
+
+// TODO - verify data.. e.g. max length.. some fields are mandatory based on others (but example contradicts spec)
+//			9303p10 - Table 71. Data Group 11 Tags
+
+type PersonDetails struct {
+	NameOfHolder         MrzName
+	OtherNames           []MrzName
+	PersonalNumber       string
+	FullDateOfBirth      string // YYYYMMDD
+	PlaceOfBirth         []string
+	Address              []string
+	Telephone            string
+	Profession           string
+	Title                string
+	PersonalSummary      string
+	ProofOfCitizenship   []byte // image (ISO-10918)
+	OtherTravelDocuments []string
+	CustodyInformation   string
+}
+
 type DG11 struct {
 	RawData []byte
+	Details PersonDetails
 }
 
 func NewDG11(data []byte) (*DG11, error) {
@@ -22,13 +49,84 @@ func NewDG11(data []byte) (*DG11, error) {
 
 	nodes := TlvDecode(out.RawData)
 
+	slog.Debug("DG11", "TLV", nodes)
+
 	rootNode := nodes.GetNode(DG11Tag)
 
 	if !rootNode.IsValidNode() {
 		return nil, fmt.Errorf("root node (%x) missing", DG11Tag)
 	}
 
-	// TODO - parse the data
+	out.Details = parseData(rootNode)
+
+	slog.Debug("DG11", "Details", out.Details)
 
 	return out, nil
+}
+
+func parseData(node TlvNode) PersonDetails {
+	var out PersonDetails
+
+	tagList := getTagList(node.GetNode(0x5C).GetValue())
+
+	for _, tag := range tagList {
+		out.processTag(tag, node)
+	}
+
+	return out
+}
+
+// TODO - also used by EF.COM... have common TLV function for this?
+func getTagList(data []byte) []TlvTag {
+	var out []TlvTag
+
+	buf := bytes.NewBuffer(data)
+
+	for {
+		if buf.Len() <= 0 {
+			break
+		}
+		out = append(out, TlvGetTag(buf))
+	}
+
+	return out
+}
+
+// processes the 'tag', getting the data from the TLV and populating PersonDetails
+func (details *PersonDetails) processTag(tag TlvTag, node TlvNode) {
+	switch tag {
+	case 0x5F0E:
+		details.NameOfHolder = parseName(decodeValue(string(node.GetNode(tag).GetValue())))
+	case 0x5F0f:
+		// special handling as 'Other Names' are nested within tag A0 and there can be multiple instances
+		numOtherNames := bytesToInt(node.GetNode(0xA0).GetNode(0x02).GetValue())
+		for occur := 1; occur <= numOtherNames; occur++ {
+			details.OtherNames = append(details.OtherNames, parseName(decodeValue(string(node.GetNode(0xA0).GetNodeByOccur(0x5f0f, occur).GetValue()))))
+		}
+	case 0x5F10:
+		details.PersonalNumber = string(node.GetNode(tag).GetValue())
+	case 0x5F2B:
+		details.FullDateOfBirth = string(node.GetNode(tag).GetValue())
+	case 0x5F11:
+		details.PlaceOfBirth = strings.Split(string(node.GetNode(tag).GetValue()), "<")
+	case 0x5F42:
+		details.Address = strings.Split(string(node.GetNode(tag).GetValue()), "<")
+	case 0x5F12:
+		details.Telephone = string(node.GetNode(tag).GetValue())
+	case 0x5F13:
+		details.Profession = decodeValue(string(node.GetNode(tag).GetValue()))
+	case 0x5F14:
+		details.Title = decodeValue(string(node.GetNode(tag).GetValue()))
+	case 0x5F15:
+		details.PersonalSummary = decodeValue(string(node.GetNode(tag).GetValue()))
+	case 0x5F16:
+		// image data
+		details.ProofOfCitizenship = node.GetNode(tag).GetValue()
+	case 0x5F17:
+		details.OtherTravelDocuments = strings.Split(string(node.GetNode(tag).GetValue()), "<")
+	case 0x5F18:
+		details.CustodyInformation = decodeValue(string(node.GetNode(tag).GetValue()))
+	default:
+		log.Panicf("Unsupported tag:%x", tag)
+	}
 }
--- dg11_test.go
+++ dg11_test.go
@@ -1,6 +1,9 @@
 package gmrtd
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestNewDG11NoData(t *testing.T) {
 	if dg11, err := NewDG11(nil); dg11 != nil || err != nil {
@@ -23,5 +26,41 @@ func TestNewDG11UnhappyRootTag(t *testing.T) {
 
 	if dg11 != nil {
 		t.Errorf("DG11 not expected for error case")
+	}
+}
+
+// (9303p10) A.5 EF.DG11 ADDITIONAL PERSONAL DETAILS
+//		** NB fixed issues in test data... e.g. 6B len (x63->x60), 5f12 len (xE->xB)
+//
+// ‘6B’ ‘60’
+// 		‘5C’ ‘0A’ ‘5F0E’ ‘5F11’ ‘5F42’ ‘5F12’ ‘5F13’
+// 		‘5F0E’ ‘0D’ SMITH<<JOHN<J								5F0E0D534D4954483C3C4A4F484E3C4A
+// 		‘5F11’ ‘0A’ ANYTOWN<MN									5F110A414E59544F574E3C4D4E
+// 		‘5F42’ ‘17’ 123 MAPLE RD<ANYTOWN<MN						5F4217313233204D41504C452052443C414E59544F574E3C4D4E
+// 		‘5F12’ ‘0B’ 16125551212									5F120B3136313235353531323132
+// 		‘5F13’ ‘0C’ TRAVEL<AGENT								5F130C54524156454C3C4147454E54
+
+// TODO - spec says that some tags are mandatory based on others (e.g. Title if Profession is present)
+//			- however the example in the spec (as above) has Profession (5F13), but not Title (5F14), so not sure how
+//			  much we can really on this
+
+func TestNewDG11Happy(t *testing.T) {
+	// TODO - 6B length changed from 63 to 60.. spec has bad TLV for sample DG11 data!
+	var dg11bytes []byte = HexToBytes("6B605C0A5F0E5F115F425F125F135F0E0D534D4954483C3C4A4F484E3C4A5F110A414E59544F574E3C4D4E5F4217313233204D41504C452052443C414E59544F574E3C4D4E5F120B31363132353535313231325F130C54524156454C3C4147454E54")
+
+	var expDetails PersonDetails = PersonDetails{NameOfHolder: MrzName{Primary: "SMITH", Secondary: "JOHN J"}, PlaceOfBirth: []string{"ANYTOWN", "MN"}, Address: []string{"123 MAPLE RD", "ANYTOWN", "MN"}, Telephone: "16125551212", Profession: "TRAVEL AGENT"}
+
+	dg11, err := NewDG11(dg11bytes)
+
+	if err != nil {
+		t.Errorf("Error not expected")
+	}
+
+	if dg11 == nil {
+		t.Errorf("DG11 expected")
+	}
+
+	if !reflect.DeepEqual(dg11.Details, expDetails) {
+		t.Errorf("DG11 PersonDetails differs to expected\n(Act:%+v)\n(Exp:%+v)", dg11.Details, expDetails)
 	}
 }
