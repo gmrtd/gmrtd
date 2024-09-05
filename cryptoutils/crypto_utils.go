3820f81b372fedca0106b36fcc23d46e7078bb4c
Oscar Sanderson <o.sanderson@gmail.com>
2024-09-05 23:47:50 +0800

    Code cleanup

--- cryptoutils/crypto_utils.go
+++ cryptoutils/crypto_utils.go
@@ -198,67 +198,6 @@ func CryptCBC(blockCipher cipher.Block, iv []byte, data []byte, encrypt bool) []
 
 }
 
-func ISO9797Method2Pad(data []byte, blockSize int) []byte {
-	paddedLen := ((len(data) + blockSize) / blockSize) * blockSize
-	out := make([]byte, paddedLen)
-	copy(out, data)
-	out[len(data)] = 0x80
-	// NB no need to add 0's to end of 'out' as it was pre-allocated to next block boundary
-	return out
-}
-
-// panics if data is not padded
-func ISO9797Method2Unpad(data []byte) []byte {
-	out := bytes.TrimRight(data, string([]byte{0}))
-	if len(out) > 0 {
-		if out[len(out)-1] == 0x80 {
-			return out[:len(out)-1]
-		}
-	}
-	log.Panicf("Data not padded according to Method-2 (%x -> %x)", data, out)
-	return nil
-}
-
-// ISO-9797 Retail MAC (DES)
-// key: 16 bytes (double) DES key
-// error: if invalid key length (not 16 bytes) or data not aligned to block boundary (8 bytes)
-func ISO9797RetailMacDes(key []byte, data []byte) (mac []byte, err error) {
-	if len(key) != 16 {
-		return nil, fmt.Errorf("key must be 16 bytes (act:%d)", len(key))
-	}
-	if (len(data) < DES_BLOCK_SIZE_BYTES) || (len(data)%DES_BLOCK_SIZE_BYTES != 0) {
-		return nil, fmt.Errorf("data must consist of 1 or more blocks (BlockSize:%d, ActLen:%d)", DES_BLOCK_SIZE_BYTES, len(data))
-	}
-
-	k1 := make([]byte, 8)
-	k2 := make([]byte, 8)
-
-	copy(k1, key[0:8])
-	copy(k2, key[8:16])
-
-	var cipherK1 cipher.Block
-	var cipherK2 cipher.Block
-
-	if cipherK1, err = GetCipherForKey(DES, k1); err != nil {
-		return nil, err
-	}
-	if cipherK2, err = GetCipherForKey(DES, k2); err != nil {
-		return nil, err
-	}
-
-	tmp := CryptCBC(cipherK1, make([]byte, DES_BLOCK_SIZE_BYTES), data, true)
-
-	// get last block (8 bytes)
-	cbcBlock1 := make([]byte, DES_BLOCK_SIZE_BYTES)
-	copy(cbcBlock1, tmp[len(tmp)-DES_BLOCK_SIZE_BYTES:])
-
-	cbcBlock2 := CryptCBC(cipherK2, make([]byte, DES_BLOCK_SIZE_BYTES), cbcBlock1, false)
-
-	mac = CryptCBC(cipherK1, make([]byte, DES_BLOCK_SIZE_BYTES), cbcBlock2, true)
-
-	return mac, nil
-}
-
 // Maps hash algorithm OIDs to crypto.Hash values
 var oidHashAlgorithmToCryptoHash = map[string]crypto.Hash{
 	oid.OidHashAlgorithmMD5.String():    crypto.MD5,
---
+++ cryptoutils/iso9797_test.go
@@ -0,0 +1,144 @@
+package cryptoutils
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/gmrtd/gmrtd/utils"
+)
+
+func TestISO9797Method2Pad(t *testing.T) {
+	testCases := []struct {
+		data      []byte
+		blockSize int
+		exp       []byte
+	}{
+		{
+			data:      utils.HexToBytes("1234567880"),
+			blockSize: 8,
+			exp:       utils.HexToBytes("1234567880800000"),
+		},
+		{
+			data:      utils.HexToBytes("1234"),
+			blockSize: 8,
+			exp:       utils.HexToBytes("1234800000000000"),
+		},
+		{
+			data:      utils.HexToBytes(""),
+			blockSize: 8,
+			exp:       utils.HexToBytes("8000000000000000"),
+		},
+		{
+			data:      utils.HexToBytes("1234567890ABCDEF"),
+			blockSize: 8,
+			exp:       utils.HexToBytes("1234567890ABCDEF8000000000000000"),
+		},
+	}
+	for _, tc := range testCases {
+		out := ISO9797Method2Pad(tc.data, tc.blockSize)
+
+		if !bytes.Equal(tc.exp, out) {
+			t.Errorf("Pad failed (Exp:%x) (Act:%x)", tc.exp, out)
+		}
+	}
+}
+
+func TestISO9797Method2Unpad(t *testing.T) {
+	testCases := []struct {
+		in  []byte
+		exp []byte
+	}{
+		{
+			in:  utils.HexToBytes("123456788080"),
+			exp: utils.HexToBytes("1234567880"),
+		},
+		{
+			in:  utils.HexToBytes("12348000000000"),
+			exp: utils.HexToBytes("1234"),
+		},
+		{
+			in:  utils.HexToBytes("800000"),
+			exp: utils.HexToBytes(""),
+		},
+	}
+	for _, tc := range testCases {
+		actBytes := ISO9797Method2Unpad(tc.in)
+
+		if !bytes.Equal(tc.exp, actBytes) {
+			t.Errorf("Unpad failed (Exp:%x) (Act:%x)", tc.exp, actBytes)
+		}
+	}
+}
+
+func TestISO9797RetailMacDesErrors(t *testing.T) {
+	// NB: ISO9797 Retail MAC requires 16-byte DES key
+	testCases := []struct {
+		key  []byte
+		data []byte
+	}{
+		{
+			// bad key length (8 bytes instead of 16)
+			key:  utils.HexToBytes("0123456789ABCDEF"),
+			data: utils.HexToBytes("0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF"),
+		},
+		{
+			// bad key length (24 bytes instead of 16)
+			key:  utils.HexToBytes("0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF"),
+			data: utils.HexToBytes("0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF"),
+		},
+		{
+			// data not aligned with block boundary (ie not multiple of 8 bytes)
+			key:  utils.HexToBytes("0123456789ABCDEF0123456789ABCDEF"),
+			data: utils.HexToBytes("0123456789ABCDEF01"),
+		},
+	}
+	for _, tc := range testCases {
+		mac, err := ISO9797RetailMacDes(tc.key, tc.data)
+
+		if err == nil {
+			t.Errorf("Error expected")
+		}
+
+		if mac != nil {
+			t.Errorf("MAC not expected for error case")
+		}
+	}
+}
+
+func TestISO9797RetailMacDes(t *testing.T) {
+	testCases := []struct {
+		key    []byte
+		data   []byte
+		expMac []byte
+	}{
+		{
+			key:    utils.HexToBytes("e3857ca24946251c151c540e13f2cd51"),
+			data:   utils.HexToBytes("00000000000000cd0c88000080000000871101ed7f8cb47a4eea086324a7f9dd7427809701008000"),
+			expMac: utils.HexToBytes("97f54bae49aa71f8"),
+		},
+		{
+			key:    utils.HexToBytes("7962d9ece03d1acd4c76089dce131543"),
+			data:   utils.HexToBytes("46b9342a41396cd7386bf5803104d7cedc122b9132139baf2eedc94ee178534f8000000000000000"),
+			expMac: utils.HexToBytes("2f2d235d074d7449"),
+		},
+		{
+			key:    utils.HexToBytes("d6c47ff4677ac8ae91cb49f4ce673432"),
+			data:   utils.HexToBytes("9646c154bfb7be7c0c86000080000000874901a9c6bf370c62aa43ca1ec9b97071727654822361deb8ba4e5719aa05d8e86aa36164dfa5e506dbf60dc7418858179fd25dac5e9e49393dde37e29652444941b562142ab5f19aea919701008000"),
+			expMac: utils.HexToBytes("5083316637060c4d"),
+		},
+		{
+			key:    utils.HexToBytes("c1bc1f075797b970b5a45e64a764b0cb"),
+			data:   utils.HexToBytes("7f494f060a04007f000702020402018641042dbaf62e6fdfb31eb66f206493b9e7721586f0e5c93754d7bd5a884ca251ee4d720ab539a60561bc46812fa289b58ac69f0c6e32adbaf7241049a31211f80f5b800000000000"),
+			expMac: utils.HexToBytes("8d7c617d43efe09e"),
+		},
+	}
+	for _, tc := range testCases {
+		mac, err := ISO9797RetailMacDes(tc.key, tc.data)
+
+		if err != nil {
+			t.Errorf("Unexpected error: %s", err)
+		} else if !bytes.Equal(mac, tc.expMac) {
+			t.Errorf("MAC differs to expected (exp:%x) (act:%x)", tc.expMac, mac)
+		}
+	}
+}
