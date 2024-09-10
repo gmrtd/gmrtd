d1d565d7650370380d3b0e48b45072a11c8ed654
Oscar Sanderson <o.sanderson@gmail.com>
2024-09-10 21:16:16 +0800

    Utils UT tweaks

--- utils/byte_utils_test.go
+++ utils/byte_utils_test.go
@@ -200,14 +200,38 @@ func TestGetByteFromBuffer(t *testing.T) {
 }
 
 func TestIsImage(t *testing.T) {
-	// invalid image data - i.e. doesn't have a recognised image header
-	var imageBytes []byte = HexToBytes("0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF")
-
-	if IsImage(imageBytes) {
-		t.Errorf("Should NOT be an image")
+	testCases := []struct {
+		imageBytes []byte
+		isImage    bool
+	}{
+		{
+			// valid - has JPEG prefix: ffd8ffe000104a464946
+			imageBytes: HexToBytes("ffd8ffe000104a4649460000000000000000000000000000000000000000"),
+			isImage:    true,
+		},
+		{
+			// valid - has JP2 Bitmap prefix: 0000000c6a5020200d0a
+			imageBytes: HexToBytes("0000000c6a5020200d0a0000000000000000000000000000000000000000"),
+			isImage:    true,
+		},
+		{
+			// valid - has JP2 Code Stream Bitmap prefix: ff4fff51
+			imageBytes: HexToBytes("ff4fff510000000000000000000000000000000000000000"),
+			isImage:    true,
+		},
+		{
+			// invalid image data - i.e. doesn't have a recognised image header
+			imageBytes: HexToBytes("0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF"),
+			isImage:    false,
+		},
 	}
+	for _, tc := range testCases {
+		actIsImage := IsImage(tc.imageBytes)
 
-	// TODO - should have positive tests also
+		if actIsImage != tc.isImage {
+			t.Errorf("IsImage result differs to expected (exp:%t, act:%t)", tc.isImage, actIsImage)
+		}
+	}
 }
 
 func TestBytesToInt(t *testing.T) {
