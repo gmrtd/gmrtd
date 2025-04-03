87efdd5e09c4c9729407585df1dcb45a28c134a7
Oscar Sanderson <o.sanderson@gmail.com>
2025-04-03 22:02:04 +0800

    Moved 'StaticTransceiver' to separate file

--- iso7816/mock_transceiver.go
+++ iso7816/mock_transceiver.go
@@ -36,13 +36,3 @@ func (transceiver *MockTransceiver) Transceive(cla int, ins int, p1 int, p2 int,
 
 	return nil
 }
-
-// TODO - why is following not in a separate file?
-
-type StaticTransceiver struct {
-	rapdu []byte
-}
-
-func (transceiver *StaticTransceiver) Transceive(cla int, ins int, p1 int, p2 int, data []byte, le int, encodedData []byte) []byte {
-	return transceiver.rapdu
-}
