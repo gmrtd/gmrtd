d8d08ae20ba19cfd89d0f4092730eb160b3b3c0b
Oscar Sanderson <o.sanderson@gmail.com>
2025-05-20 22:04:51 +0800

    Added ISO-3166 country code support

---
+++ iso3166/countries_test.go
@@ -0,0 +1,91 @@
+package iso3166
+
+import "testing"
+
+// TODO - error cases
+
+func TestGetByAlpha2(t *testing.T) {
+	testCases := []struct {
+		alpha2    string
+		expAlpha3 string
+	}{
+		{
+			alpha2:    "SG",
+			expAlpha3: "SGP",
+		},
+	}
+	for _, tc := range testCases {
+		country := GetByAlpha2(tc.alpha2)
+
+		if country == nil {
+			t.Errorf("Unable to locate country (alpha2:%s)", tc.alpha2)
+		} else if country.Alpha3 != tc.expAlpha3 {
+			t.Errorf("Country differs to expected - alpha3 (exp:%s, act:%s)", tc.expAlpha3, country.Alpha3)
+		}
+	}
+}
+
+func TestGetByAlpha2Errors(t *testing.T) {
+	testCases := []struct {
+		alpha2 string
+	}{
+		{
+			// 'uk' is reserved, but not a valid country code
+			alpha2: "UK",
+		},
+		{
+			// fictional country (utopia) used for icao9303 test documents
+			alpha2: "UT",
+		},
+	}
+	for _, tc := range testCases {
+		country := GetByAlpha2(tc.alpha2)
+
+		if country != nil {
+			t.Errorf("Error expected (alpha2:%s)", tc.alpha2)
+		}
+	}
+}
+
+func TestGetByAlpha3(t *testing.T) {
+	testCases := []struct {
+		alpha3    string
+		expAlpha2 string
+	}{
+		{
+			alpha3:    "GBR",
+			expAlpha2: "GB",
+		},
+	}
+	for _, tc := range testCases {
+		country := GetByAlpha3(tc.alpha3)
+
+		if country == nil {
+			t.Errorf("Unable to locate country (alpha3:%s)", tc.alpha3)
+		} else if country.Alpha2 != tc.expAlpha2 {
+			t.Errorf("Country differs to expected - alpha3 (exp:%s, act:%s)", tc.expAlpha2, country.Alpha2)
+		}
+	}
+}
+
+func TestGetByAlpha3Errors(t *testing.T) {
+	testCases := []struct {
+		alpha3 string
+	}{
+		{
+			// non-existant country
+			alpha3: "ART",
+		},
+		{
+			// fictional country (utopia) used for icao9303 test documents
+			alpha3: "UTO",
+		},
+	}
+	for _, tc := range testCases {
+		country := GetByAlpha3(tc.alpha3)
+
+		if country != nil {
+			t.Errorf("Error expected (alpha3:%s)", tc.alpha3)
+		}
+	}
+}
