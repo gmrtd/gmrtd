2e6844d6f0ce81f625d96aa6100517a6f40a7b72
Oscar Sanderson <o.sanderson@gmail.com>
2025-11-12 00:48:57 +0800

    fix:incomplete image detection (#94) (#95)

--- utils/image_utils.go
+++ utils/image_utils.go
@@ -8,19 +8,38 @@ import (
 // determines whether the data constitutes an image
 // return: 'true' if image detected, otherwise 'false'
 func IsImage(imageBytes []byte) bool {
-	// TODO - review following code as it seems to support more variants...
-	//			https://gist.github.com/kvanh/76378993ed5de2182f762e19eccf36a0
+	/*
+	* Classic JPEG (JFIF/Exif/SPIF/Adobe) files begin with these bytes:
+	* 	- FF D8 — SOI
+	* 	- followed by FF and a marker (APPn, DQT, etc.)
+	* So the minimum reliable check is:
+	* 	- byte0 = 0xFF, byte1 = 0xD8, byte2 = 0xFF
+	 */
+	{
+		prefixJpeg := HexToBytes("ffd8ff")
 
-	prefixJpeg := HexToBytes("ffd8ffe000104a464946")
-	prefixJp2Bitmap := HexToBytes("0000000c6a5020200d0a")
-	prefixJp2CodestreamBitmap := HexToBytes("ff4fff51")
-
-	if !bytes.HasPrefix(imageBytes, prefixJpeg) &&
-		!bytes.HasPrefix(imageBytes, prefixJp2Bitmap) &&
-		!bytes.HasPrefix(imageBytes, prefixJp2CodestreamBitmap) {
-		slog.Debug("Unknown image type", "prefix", imageBytes[0:10])
-		return false
+		if bytes.HasPrefix(imageBytes, prefixJpeg) {
+			return true
+		}
 	}
 
-	return true
+	/*
+	* JPEG 2000 (not the same as classic JPEG):
+	* 	- JP2 file      : 00 00 00 0C 6A 50 20 20 0D 0A 87 0A
+	* 	- Raw codestream: FF 4F FF 51
+	 */
+	{
+		prefixJp2Bitmap := HexToBytes("0000000c6a5020200d0a")
+		prefixJp2CodestreamBitmap := HexToBytes("ff4fff51")
+
+		if bytes.HasPrefix(imageBytes, prefixJp2Bitmap) ||
+			bytes.HasPrefix(imageBytes, prefixJp2CodestreamBitmap) {
+			return true
+		}
+
+	}
+
+	slog.Debug("Unknown image type", "prefix", imageBytes[0:10])
+
+	return false
 }
