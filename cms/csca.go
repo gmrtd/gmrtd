defbabc30ec1b2426fe14f1a29f611387a20225c
Oscar Sanderson <o.sanderson@gmail.com>
2025-11-12 01:45:39 +0800

    feat:add support for Indonesia 2010 CSCA Series certificates (#14) (#97)

--- cms/csca.go
+++ cms/csca.go
@@ -27,6 +27,15 @@ var nl_masterListRootCA []byte
 //go:embed master_list/NL_ML_2025-07-29.ml
 var nl_masterList []byte
 
+//go:embed master_list/IDN_2010_SERIES_CSCA_CERT/2010-12_CSCA.cer
+var id_2010series_2010 []byte
+
+//go:embed master_list/IDN_2010_SERIES_CSCA_CERT/2016-01_CSCA.cer
+var id_2010series_2016 []byte
+
+//go:embed master_list/IDN_2010_SERIES_CSCA_CERT/2020-10_CSCA.cer
+var id_2010series_2020 []byte
+
 func GetDefaultMasterList() (*CombinedCertPool, error) {
 	var out CombinedCertPool
 
@@ -48,6 +57,16 @@ func GetDefaultMasterList() (*CombinedCertPool, error) {
 		out.AddCertPool(tmpCertPool)
 	}
 
+	// Indonesia: 2010 CSCA Series Certificate(s)
+	// - these are not part of ICAO PKD as Indonesia manages two separate CSCAs (see https://www.imigrasi.go.id/csca)
+	{
+		tmpCertPool, err := GetIndonesian2010SeriesCerts()
+		if err != nil {
+			return nil, fmt.Errorf("[GetDefaultMasterList] GetIndonesian2010SeriesCerts error: %w", err)
+		}
+		out.AddCertPool(tmpCertPool)
+	}
+
 	return &out, nil
 }
 
@@ -57,4 +76,30 @@ func GetGermanMasterList() (*SignedDataCertPool, error) {
 
 func GetDutchMasterList() (*SignedDataCertPool, error) {
 	return CreateCertPoolFromSignedData(nl_masterList, nl_masterListRootCA)
+}
+
+func GetIndonesian2010SeriesCerts() (*GenericCertPool, error) {
+	// Note: Indonesia manages two seperate CSCAs (2010/2018 series)!
+	//
+	// Only the 2018 series is published via ICAO PKD (and NL Master-List).
+	//
+	// As such we need to directly load the 2010 Series (for which there will not
+	// be any new CSCA certs issued) based on the certs published at:
+	// - https://www.imigrasi.go.id/csca
+	var certPool GenericCertPool
+	var err error
+
+	if err = certPool.Add(id_2010series_2010); err != nil {
+		return nil, fmt.Errorf("[GetIndonesian2010SeriesCerts] Error adding 2010 cert: %w", err)
+	}
+
+	if err = certPool.Add(id_2010series_2016); err != nil {
+		return nil, fmt.Errorf("[GetIndonesian2010SeriesCerts] Error adding 2016 cert: %w", err)
+	}
+
+	if err = certPool.Add(id_2010series_2020); err != nil {
+		return nil, fmt.Errorf("[GetIndonesian2010SeriesCerts] Error adding 2020 cert: %w", err)
+	}
+
+	return &certPool, nil
 }
