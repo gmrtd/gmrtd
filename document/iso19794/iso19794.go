b74cd7643a70757de3e855aa276aa90df546bbc5
Oscar Sanderson <o.sanderson@gmail.com>
2026-03-28 14:05:32 +0800

    refactor(iso19794): harden ISO19794 parser and add unit tests

--- document/iso19794/iso19794.go
+++ document/iso19794/iso19794.go
@@ -4,12 +4,20 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"io"
+	"log/slog"
 
 	"github.com/gmrtd/gmrtd/utils"
 )
 
 // the following structures are largely borrowed from:
 //	https://github.com/paultag/go-cbeff
+
+// sane limits
+const (
+	MaxImages         = 4
+	MaxFacialFeatures = 32
+)
 
 type ISO19794 struct {
 	Facial FacialType `json:"facial,omitempty"`
@@ -72,13 +80,19 @@ func ProcessISO19794(data []byte) (*ISO19794, error) {
 	fh := FacialHeader{}
 	{
 		if err := binary.Read(r, binary.BigEndian, &fh); err != nil {
-			return nil, fmt.Errorf("[processISO19794] binary.Read error: %w", err)
+			return nil, fmt.Errorf("[ProcessISO19794] binary.Read error: %w", err)
 		}
 		if !bytes.Equal(fh.FormatID[:], []byte{0x46, 0x41, 0x43, 0}) {
-			return nil, fmt.Errorf("[processISO19794] Invalid FacialHeader.FormatID (Act:%x)", fh.FormatID)
+			return nil, fmt.Errorf("[ProcessISO19794] Invalid FacialHeader.FormatID (Act:%x)", fh.FormatID)
 		}
 	}
 
+	var err error
+	var out ISO19794
+
+	out.Facial.Header = fh
+
+	// sanity check that RecordLength matches the amount of data we're processing
 	if fh.RecordLength != uint32(len(data)) {
 		// NB we've seen slightly different record-lengths in various passports:
 		//    - NZ passport: hdr.recordLength = dataLen - 8
@@ -90,25 +104,22 @@ func ProcessISO19794(data []byte) (*ISO19794, error) {
 		}
 	}
 
-	var out ISO19794
-
-	out.Facial.Header = fh
-
-	// read images
-	for i := 0; i < int(out.Facial.Header.NumberOfFaces); i++ {
-		image, err := parseImageIso19794(r)
-		if err != nil {
-			return nil, fmt.Errorf("[processISO19794] parseImageIso19794(i:%1d) error: %w", i, err)
-		}
-		out.Facial.Images = append(out.Facial.Images, *image)
+	out.Facial.Images, err = parseImages(uint32(fh.NumberOfFaces), r)
+	if err != nil {
+		return nil, fmt.Errorf("[ProcessISO19794] parseImages error: %w", err)
 	}
 
 	// verify that we read all of the data (or close to it)
 	// Note: Some passports have small discrepancies in RecordLength (1-8 bytes), so we tolerate up to 8 bytes remaining
+	//    		- NZ passport: hdr.recordLength = dataLen - 8
+	//    		- UK passport: hdr.recordLength = dataLen - 1
 	{
 		remaining := r.Len()
-		if remaining > 8 {
-			return nil, fmt.Errorf("[processISO19794] Too much data remaining: expected at most 8 bytes, got %d", remaining)
+
+		if remaining <= 8 {
+			slog.Warn("ProcessISO19794 - tolerating excess trailing bytes", "excess-bytes", remaining)
+		} else {
+			return nil, fmt.Errorf("[ProcessISO19794] Too much data remaining: expected at most 8 bytes, got %d", remaining)
 		}
 	}
 
@@ -118,43 +129,78 @@ func ProcessISO19794(data []byte) (*ISO19794, error) {
 func (ap ISO19794) Images() [][]byte {
 	var out [][]byte = make([][]byte, 0)
 
+	out = make([][]byte, len(ap.Facial.Images))
+
 	for i := range ap.Facial.Images {
-		out = append(out, bytes.Clone(ap.Facial.Images[i].Data))
+		out[i] = bytes.Clone(ap.Facial.Images[i].Data)
 	}
 
 	return out
 }
 
-func parseImageIso19794(r *bytes.Reader) (*Image, error) {
-	fi := FacialInfo{}
-	if err := binary.Read(r, binary.BigEndian, &fi); err != nil {
-		return nil, fmt.Errorf("[parseImageIso19794] binary.Read(FacialInfo) error: %w", err)
+func parseImages(numImages uint32, r *bytes.Reader) ([]Image, error) {
+	var out []Image
+
+	// sanity check
+	if numImages > MaxImages {
+		return nil, fmt.Errorf("[parseImages] too many images (act:%d, max:%d)", numImages, MaxImages)
 	}
 
-	features := []FacialFeature{}
-	for i := 0; i < int(fi.NumberOfPoints); i++ {
-		feature := FacialFeature{}
-		if err := binary.Read(r, binary.BigEndian, &feature); err != nil {
-			return nil, fmt.Errorf("[parseImageIso19794] binary.Read(FacialFeature:i:%1d) error: %w", i, err)
+	out = make([]Image, numImages)
+
+	// read images
+	var i uint32
+	for i = 0; i < numImages; i++ {
+		image, err := parseImage(r)
+		if err != nil {
+			return nil, fmt.Errorf("[processISO19794] parseImageIso19794(i:%1d) error: %w", i, err)
 		}
-		features = append(features, feature)
+		out[i] = *image
+	}
+
+	return out, nil
+}
+
+func parseImage(r *bytes.Reader) (*Image, error) {
+	fi := FacialInfo{}
+	if err := binary.Read(r, binary.BigEndian, &fi); err != nil {
+		return nil, fmt.Errorf("[parseImage] binary.Read(FacialInfo) error: %w", err)
+	}
+
+	features, err := parseFeatures(uint32(fi.NumberOfPoints), r)
+	if err != nil {
+		return nil, fmt.Errorf("[parseImage] parseFeatures error: %w", err)
 	}
 
 	ii := ImageInfo{}
 	if err := binary.Read(r, binary.BigEndian, &ii); err != nil {
-		return nil, fmt.Errorf("[parseImageIso19794] binary.Read(ImageInfo) error: %w", err)
+		return nil, fmt.Errorf("[parseImage] binary.Read(ImageInfo) error: %w", err)
 	}
 
-	// image-size = fi.Length - FacialInfo [20 bytes] - ImageInfo [12 bytes] - FacialFeatures(s) [* 8 bytes]
-	expImageSize := fi.Length - 20 - 12 - (uint32(fi.NumberOfPoints) * 8)
-	imageBytes := make([]byte, expImageSize)
-	err := binary.Read(r, binary.BigEndian, imageBytes)
+	// determine the minimum valid 'Length'
+	// --> FacialInfo[20 bytes] + ImageInfo[12 bytes] + FacialFeatures(s)[NumberOfPoints * 8 bytes]
+	var minFiLength uint32 = 20 + 12 + (uint32(fi.NumberOfPoints) * 8)
+
+	// sanity check to prevent unsafe FacialInfo.Length (i.e. < minFiLength)
+	if fi.Length < minFiLength {
+		return nil, fmt.Errorf("[parseImage] FacialInfo.Length smaller than allowed minimum (fi.length:%d, min:%d)", fi.Length, minFiLength)
+	}
+
+	var imageSize uint32 = fi.Length - minFiLength
+
+	// sanity check before allocating memory, 'imageSize' cannot exceed remaining bytes
+	if imageSize > uint32(r.Len()) {
+		return nil, fmt.Errorf("[parseImage] Image Size cannot be more than remaining data (size:%d, remaining:%d)", imageSize, r.Len())
+	}
+
+	imageBytes := make([]byte, imageSize)
+	_, err = io.ReadFull(r, imageBytes)
 	if err != nil {
-		return nil, fmt.Errorf("[parseImageIso19794] binary.Read(imageBytes) error: %w", err)
+		return nil, fmt.Errorf("[parseImage] io.ReadFull(imageBytes) error: %w", err)
 	}
 
 	if !utils.IsImage(imageBytes) {
-		return nil, fmt.Errorf("[parseImageIso19794] Unknown image type [prefixBytes:%x]", utils.SafePrefix(imageBytes, 10))
+		return nil, fmt.Errorf("[parseImage] Unknown image type [prefixBytes:%x]", utils.SafePrefix(imageBytes, 10))
 	}
 
 	var img Image = Image{
@@ -165,4 +211,26 @@ func parseImageIso19794(r *bytes.Reader) (*Image, error) {
 	}
 
 	return &img, nil
+}
+
+func parseFeatures(numFeatures uint32, r *bytes.Reader) ([]FacialFeature, error) {
+	var out []FacialFeature
+
+	// sanity check
+	if numFeatures > MaxFacialFeatures {
+		return nil, fmt.Errorf("[parseFeatures] too many facial-features (act:%d, max:%d)", numFeatures, MaxFacialFeatures)
+	}
+
+	out = make([]FacialFeature, numFeatures)
+
+	var i uint32
+	for i = 0; i < numFeatures; i++ {
+		feature := FacialFeature{}
+		if err := binary.Read(r, binary.BigEndian, &feature); err != nil {
+			return nil, fmt.Errorf("[parseFeatures] binary.Read(i:%1d) error: %w", i, err)
+		}
+		out[i] = feature
+	}
+
+	return out, nil
 }
--- document/iso19794/iso19794_test.go
+++ document/iso19794/iso19794_test.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	_ "embed" // Import for loading test files
 	"testing"
+
+	"github.com/gmrtd/gmrtd/utils"
 )
 
 //go:embed test_data/ISO19794_Test1_Data.bin
@@ -40,16 +42,48 @@ func TestProcessISO19794InvalidImage(t *testing.T) {
 	}
 }
 
-func TestProcessISO19794RecordLengthValidation(t *testing.T) {
+func TestProcessISO19794Permutations(t *testing.T) {
+	// FacialHeader(valid)
+	var facialHeaderValid []byte = utils.HexToBytes("4641430030313000000014190001")
+
+	// FacialInfo(valid)
+	var facialInfoValid []byte = utils.HexToBytes("0000140b00000000000000000000000000000000")
+
+	// FacialFeature(valid)
+	var facialFeatureValid []byte = utils.HexToBytes("0000000000000000")
+
+	// ImageInfo(valid)
+	var imageInfoValid []byte = utils.HexToBytes("020100940046000000000000")
+
 	// Test cases for recordLength and dataLength differences
 	testCases := []struct {
 		name          string
+		data          []byte
 		modifyData    func([]byte) []byte
 		expectError   bool
 		errorContains string
 	}{
 		{
+			name: "Invalid: Image is shorter than expected (by 1 byte)",
+			data: test1data,
+			modifyData: func(data []byte) []byte {
+				testData := bytes.Clone(data)
+				// remove last byte (from image)
+				testData = testData[:len(testData)-1]
+				// update length
+				dataLen := len(testData)
+				testData[8] = byte(dataLen >> 24)
+				testData[9] = byte(dataLen >> 16)
+				testData[10] = byte(dataLen >> 8)
+				testData[11] = byte(dataLen)
+				return testData
+			},
+			expectError:   true,
+			errorContains: "[parseImage] Image Size cannot be more than remaining data",
+		},
+		{
 			name: "Valid: recordLength equals dataLength",
+			data: test1data,
 			modifyData: func(data []byte) []byte {
 				testData := bytes.Clone(data)
 				// recordLength is at bytes 8-11 (uint32, big endian)
@@ -64,6 +98,7 @@ func TestProcessISO19794RecordLengthValidation(t *testing.T) {
 		},
 		{
 			name: "Valid: recordLength is dataLength - 1",
+			data: test1data,
 			modifyData: func(data []byte) []byte {
 				testData := bytes.Clone(data)
 				// Set recordLength to dataLength - 1
@@ -78,6 +113,7 @@ func TestProcessISO19794RecordLengthValidation(t *testing.T) {
 		},
 		{
 			name: "Valid: recordLength is dataLength - 8",
+			data: test1data,
 			modifyData: func(data []byte) []byte {
 				testData := bytes.Clone(data)
 				// Set recordLength to dataLength - 8
@@ -92,6 +128,7 @@ func TestProcessISO19794RecordLengthValidation(t *testing.T) {
 		},
 		{
 			name: "Invalid: recordLength is dataLength - 9",
+			data: test1data,
 			modifyData: func(data []byte) []byte {
 				testData := bytes.Clone(data)
 				// Set recordLength to dataLength - 9 (exceeds tolerance)
@@ -107,6 +144,7 @@ func TestProcessISO19794RecordLengthValidation(t *testing.T) {
 		},
 		{
 			name: "Invalid: recordLength is greater than dataLength",
+			data: test1data,
 			modifyData: func(data []byte) []byte {
 				testData := bytes.Clone(data)
 				// Set recordLength to dataLength + 1
@@ -122,6 +160,7 @@ func TestProcessISO19794RecordLengthValidation(t *testing.T) {
 		},
 		{
 			name: "Invalid: recordLength is dataLength + 10",
+			data: test1data,
 			modifyData: func(data []byte) []byte {
 				testData := bytes.Clone(data)
 				// Set recordLength to dataLength + 10
@@ -135,11 +174,188 @@ func TestProcessISO19794RecordLengthValidation(t *testing.T) {
 			expectError:   true,
 			errorContains: "FacialHeader.RecordLength does not match with data",
 		},
+		{
+			name: "Invalid: FacialHeader too short (1 byte less than expected)",
+			data: facialHeaderValid,
+			modifyData: func(data []byte) []byte {
+				testData := bytes.Clone(data)
+				// reduce size by 1 byte to trigger parsing error for FacialHeader
+				return testData[:len(testData)-1]
+			},
+			expectError:   true,
+			errorContains: "[ProcessISO19794] binary.Read error: unexpected EOF",
+		},
+		{
+			name: "Invalid: FacialHeader.FormatID differs to expected (0x46->0xff)",
+			data: facialHeaderValid,
+			modifyData: func(data []byte) []byte {
+				testData := bytes.Clone(data)
+				// invalidate FacialHeader.FormatID
+				testData[0] = 0xff
+				return testData
+			},
+			expectError:   true,
+			errorContains: "[ProcessISO19794] Invalid FacialHeader.FormatID",
+		},
+		{
+			name: "Invalid: FacialHeader.NumberOfFaces too large (act:65535, max:4)",
+			data: facialHeaderValid,
+			modifyData: func(data []byte) []byte {
+				testData := bytes.Clone(data)
+				// set NumberOfFaces to 0xffff (65535)
+				testData[12] = 0xff
+				testData[13] = 0xff
+				// update length
+				dataLen := len(testData)
+				testData[8] = byte(dataLen >> 24)
+				testData[9] = byte(dataLen >> 16)
+				testData[10] = byte(dataLen >> 8)
+				testData[11] = byte(dataLen)
+				return testData
+			},
+			expectError:   true,
+			errorContains: "parseImages error: [parseImages] too many images",
+		},
+		{
+			name: "Invalid: FacialInfo too short (1 byte less than expected)",
+			data: append(facialHeaderValid, facialInfoValid...),
+			modifyData: func(data []byte) []byte {
+				testData := bytes.Clone(data)
+				// reduce size by 1 byte to trigger parsing error for FacialInfo
+				testData = testData[:len(testData)-1]
+				// update length
+				dataLen := len(testData)
+				testData[8] = byte(dataLen >> 24)
+				testData[9] = byte(dataLen >> 16)
+				testData[10] = byte(dataLen >> 8)
+				testData[11] = byte(dataLen)
+				return testData
+			},
+			expectError:   true,
+			errorContains: "[parseImage] binary.Read(FacialInfo) error: unexpected EOF",
+		},
+		{
+			name: "Invalid: FacialFeature present, but data after is missing (testing FacialFeature recording)",
+			data: append(append(facialHeaderValid, facialInfoValid...), facialFeatureValid...),
+			modifyData: func(data []byte) []byte {
+				testData := bytes.Clone(data)
+				// indicate 1 FacialFeature present
+				testData[14+4] = 0x00
+				testData[14+5] = 0x01
+				// update length
+				dataLen := len(testData)
+				testData[8] = byte(dataLen >> 24)
+				testData[9] = byte(dataLen >> 16)
+				testData[10] = byte(dataLen >> 8)
+				testData[11] = byte(dataLen)
+				return testData
+			},
+			expectError:   true,
+			errorContains: "[parseImage] binary.Read(ImageInfo) error: EOF",
+		},
+		{
+			name: "Invalid: FacialFeature present, but too many features advertised (act:65535, max:32)",
+			data: append(append(facialHeaderValid, facialInfoValid...), facialFeatureValid...),
+			modifyData: func(data []byte) []byte {
+				testData := bytes.Clone(data)
+				// indicate 1 FacialFeature present
+				testData[14+4] = 0xff
+				testData[14+5] = 0xff
+				// update length
+				dataLen := len(testData)
+				testData[8] = byte(dataLen >> 24)
+				testData[9] = byte(dataLen >> 16)
+				testData[10] = byte(dataLen >> 8)
+				testData[11] = byte(dataLen)
+				return testData
+			},
+			expectError:   true,
+			errorContains: "[parseFeatures] too many facial-features",
+		},
+		{
+			name: "Invalid: FacialFeature present, but less bytes than expected",
+			data: append(append(facialHeaderValid, facialInfoValid...), facialFeatureValid...),
+			modifyData: func(data []byte) []byte {
+				testData := bytes.Clone(data)
+				// indicate 1 FacialFeature present
+				testData[14+4] = 0x00
+				testData[14+5] = 0x01
+				// reduce size by 1 byte to trigger parsing error for FacialFeature
+				testData = testData[:len(testData)-1]
+				// update length
+				dataLen := len(testData)
+				testData[8] = byte(dataLen >> 24)
+				testData[9] = byte(dataLen >> 16)
+				testData[10] = byte(dataLen >> 8)
+				testData[11] = byte(dataLen)
+				return testData
+			},
+			expectError:   true,
+			errorContains: "[parseFeatures] binary.Read(i:0) error: unexpected EOF",
+		},
+		{
+			name: "Invalid: Missing ImageInfo",
+			data: append(facialHeaderValid, facialInfoValid...),
+			modifyData: func(data []byte) []byte {
+				testData := bytes.Clone(data)
+				// update length
+				dataLen := len(testData)
+				testData[8] = byte(dataLen >> 24)
+				testData[9] = byte(dataLen >> 16)
+				testData[10] = byte(dataLen >> 8)
+				testData[11] = byte(dataLen)
+				return testData
+			},
+			expectError:   true,
+			errorContains: "[parseImage] binary.Read(ImageInfo) error: EOF",
+		},
+		{
+			name: "Invalid: Missing image",
+			data: append(append(facialHeaderValid, facialInfoValid...), imageInfoValid...),
+			modifyData: func(data []byte) []byte {
+				testData := bytes.Clone(data)
+				// update length
+				dataLen := len(testData)
+				testData[8] = byte(dataLen >> 24)
+				testData[9] = byte(dataLen >> 16)
+				testData[10] = byte(dataLen >> 8)
+				testData[11] = byte(dataLen)
+				return testData
+			},
+			expectError:   true,
+			errorContains: "[parseImage] Image Size cannot be more than remaining data",
+		},
+		{
+			name: "Invalid: Malicious FacialInfo.Length (must be > 32 bytes)",
+			data: append(append(facialHeaderValid, facialInfoValid...), imageInfoValid...),
+			modifyData: func(data []byte) []byte {
+				testData := bytes.Clone(data)
+				// change FacialInfo.Length to < 32 (set to 31 -> 0x0000001F)
+				testData[14] = 0x00
+				testData[15] = 0x00
+				testData[16] = 0x00
+				testData[17] = 0x1F
+				// update length
+				dataLen := len(testData)
+				testData[8] = byte(dataLen >> 24)
+				testData[9] = byte(dataLen >> 16)
+				testData[10] = byte(dataLen >> 8)
+				testData[11] = byte(dataLen)
+				return testData
+			},
+			expectError:   true,
+			errorContains: "[parseImage] FacialInfo.Length smaller than allowed minimum",
+		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			testData := tc.modifyData(test1data)
+			testData := bytes.Clone(tc.data)
+
+			if tc.modifyData != nil {
+				testData = tc.modifyData(testData)
+			}
+
 			_, err := ProcessISO19794(testData)
 
 			if tc.expectError {
