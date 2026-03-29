978c8c134dbec527ea0b06818a26727ed9f443dd
Oscar Sanderson <o.sanderson@gmail.com>
2026-03-29 11:08:18 +0800

    test(dg16):increased unit test coverage

--- document/dg16_test.go
+++ document/dg16_test.go
@@ -1,6 +1,7 @@
 package document
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 
@@ -89,6 +90,63 @@ func TestNewDG16Happy(t *testing.T) {
 
 		if !reflect.DeepEqual(doc.Mf.Lds1.Dg16.PersonsToNotify, tc.expPersons) {
 			t.Errorf("DG16 PersonsToNotify differs to expected\n(Act:%+v)\n(Exp:%+v)", doc.Mf.Lds1.Dg16.PersonsToNotify, tc.expPersons)
+		}
+	}
+}
+
+func TestNewDG16Errors(t *testing.T) {
+	// (9303p10) A.6 EF.DG16 PERSON(S) TO NOTIFY
+	//
+	// ‘70’ ‘81A2’
+	// 		‘02’ ‘01’ 2
+	// 		‘A1’ ‘4C’
+	// 			‘5F50’ ‘08’ 20020101								5F50083230303230313031
+	// 			‘5F51’ ‘10’ SMITH<<CHARLES<R						5F5110534D4954483C3C434841524C45533C52
+	// 			‘5F52’ ‘0B’ 19525551212								5F520B3139353235353531323132
+	// 			‘5F53’ ‘1D’ 123 MAPLE RD<ANYTOWN<MN<55100			5F531D313233204D41504C452052443C414E59544F574E3C4D4E3C3535313030
+	// 		‘A2’ ‘4F’
+	// 			‘5F50’ ‘08’ 20020315								5F50083230303230333135
+	// 			‘5F51’ ‘0D’ BROWN<<MARY<J							5F510D42524F574E3C3C4D4152593C4A
+	// 			‘5F52’ ‘0B’ 14155551212								5F520B3134313535353531323132
+	// 			‘5F53’ ‘23’ 49 REDWOOD LN<OCEAN BREEZE<CA<94000		5F5323343920524544574F4F44204C4E3C4F4345414E20425245455A453C43413C3934303030
+	//
+	// --> 7081A2020102A14C5F500832303032303130315F5110534D4954483C3C434841524C45533C525F520B31393532353535313231325F531D313233204D41504C452052443C414E59544F574E3C4D4E3C3535313030A24F5F500832303032303331355F510D42524F574E3C3C4D4152593C4A5F520B31343135353535313231325F5323343920524544574F4F44204C4E3C4F4345414E20425245455A453C43413C3934303030
+
+	testCases := []struct {
+		dg16bytes     []byte
+		errorContains string
+	}{
+		{
+			// error: more templates advertised than are actually present (3 vs 2)
+			dg16bytes:     utils.HexToBytes("7081A2020103A14C5F500832303032303130315F5110534D4954483C3C434841524C45533C525F520B31393532353535313231325F531D313233204D41504C452052443C414E59544F574E3C4D4E3C3535313030A24F5F500832303032303331355F510D42524F574E3C3C4D4152593C4A5F520B31343135353535313231325F5323343920524544574F4F44204C4E3C4F4345414E20425245455A453C43413C3934303030"),
+			errorContains: "template (a3) expected based on numTemplates tag (3)",
+		},
+		{
+			// error: invalid number of templates advertised (0) - valid range is 1..15
+			dg16bytes:     utils.HexToBytes("7003020100"),
+			errorContains: "numTemplates (0) must be between 1 and 15(xF)",
+		},
+		{
+			// error: invalid number of templates advertised (16) - valid range is 1..15
+			dg16bytes:     utils.HexToBytes("7003020110"),
+			errorContains: "numTemplates (16) must be between 1 and 15(xF)",
+		},
+		{
+			// error: invalid name (3 components) in 2nd template
+			//			- BROWN<<MARY<J -> BROWN<<MAR<<J
+			dg16bytes:     utils.HexToBytes("7081A2020102A14C5F500832303032303130315F5110534D4954483C3C434841524C45533C525F520B31393532353535313231325F531D313233204D41504C452052443C414E59544F574E3C4D4E3C3535313030A24F5F500832303032303331355F510D42524F574E3C3C4D41523C3C4A5F520B31343135353535313231325F5323343920524544574F4F44204C4E3C4F4345414E20425245455A453C43413C3934303030"),
+			errorContains: "mrz.ParseName error: [ParseName] Incorrect number of name components: 3",
+		},
+	}
+	for _, tc := range testCases {
+		var doc Document
+
+		err := doc.NewDG(16, tc.dg16bytes)
+
+		if err == nil {
+			t.Errorf("Error expected")
+		} else if !bytes.Contains([]byte(err.Error()), []byte(tc.errorContains)) {
+			t.Errorf("Expected error to contain '%s', got: %s", tc.errorContains, err.Error())
 		}
 	}
 }
