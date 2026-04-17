7b3f21a3eeca576c3539c3e502665ff62bee88c6
Oscar Sanderson <o.sanderson@gmail.com>
2026-04-17 23:35:53 +0800

    feat(csca):update DE CSCA Master-List (fixes #241)

--- cms/csca.go
+++ cms/csca.go
@@ -21,7 +21,7 @@ var (
 //go:embed master_list/DE_ROOT_CA_CSCA07.cer
 var de_masterListRootCA []byte
 
-//go:embed master_list/DE_ML_2026-01-08-12-20-54.ml
+//go:embed master_list/DE_ML_2026-04-08-14-24-32.ml
 var de_masterList []byte
 
 /*
