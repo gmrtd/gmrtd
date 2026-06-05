5dbbe6e30b5c9aa9ae62890bf1bba649a15a6f4d
Oscar Sanderson <o.sanderson@gmail.com>
2026-06-05 18:39:46 +0800

    fix(tlv):tlv.Decode unbounded recursion and O(n²) buffer cloning  (#341)

--- tlv/decode.go
+++ tlv/decode.go
@@ -7,95 +7,85 @@ import (
 	"github.com/gmrtd/gmrtd/utils"
 )
 
-// internal decode function - clients should use TlvDecode
-func decode(data []byte) (nodes *TlvNodes, remainingData []byte, err error) {
+const (
+	maxDecodeDepth = 50
+	maxDecodeNodes = 10000
+)
+
+func decodeFromBuffer(buf *bytes.Buffer, depth int, nodeCount *int) (nodes *TlvNodes, err error) {
+	if depth > maxDecodeDepth {
+		return nil, fmt.Errorf("[decode] exceeded maximum nesting depth (%d)", maxDecodeDepth)
+	}
+
 	nodes = &TlvNodes{}
 
-	buf := bytes.NewBuffer(data)
-
-	for {
-		if buf.Len() <= 0 {
-			break
-		}
-
+	for buf.Len() > 0 {
 		tag, length, err := ParseTagAndLength(buf)
 		if err != nil {
-			return nil, nil, fmt.Errorf("[decode] ParseTagAndLength error: %w", err)
+			return nil, fmt.Errorf("[decode] ParseTagAndLength error: %w", err)
 		}
 
-		// special handling for indefinite-length mode end sentinel (i.e. 0x0000)
 		if tag == 0 && length == 0 {
-			remainingData = bytes.Clone(buf.Bytes())
-			break
+			return nodes, nil
+		}
+
+		*nodeCount++
+		if *nodeCount > maxDecodeNodes {
+			return nil, fmt.Errorf("[decode] exceeded maximum node count (%d)", maxDecodeNodes)
 		}
 
 		if tag.IsConstructed() {
 			var children *TlvNodes
 
-			if length == -1 { // indefinite-length
-				childData := bytes.Clone(buf.Bytes())
-				buf = bytes.NewBuffer([]byte{})
-
-				children, remainingData, err = decode(childData)
+			if length == -1 {
+				children, err = decodeFromBuffer(buf, depth+1, nodeCount)
 				if err != nil {
-					return nil, nil, fmt.Errorf("[decode] error: %w", err)
-				}
-
-				node := NewTlvConstructedNode(tag)
-				node.children.AddNodes(*children)
-
-				nodes.AddNode(node)
-
-				// we may or may not have remaining-data
-				// if we do, then it needs to be processed as siblings for this node
-				if len(remainingData) > 0 {
-					buf = bytes.NewBuffer(remainingData)
-					remainingData = nil
+					return nil, fmt.Errorf("[decode] error: %w", err)
 				}
 			} else {
 				childData, err := utils.BytesFromBuffer(buf, int(length))
 				if err != nil {
-					return nil, nil, fmt.Errorf("[decode] ByteBuffer error: %w", err)
+					return nil, fmt.Errorf("[decode] ByteBuffer error: %w", err)
 				}
-				children, remainingData, err = decode(childData)
+				childBuf := bytes.NewBuffer(childData)
+				children, err = decodeFromBuffer(childBuf, depth+1, nodeCount)
 				if err != nil {
-					return nil, nil, fmt.Errorf("[decode] error: %w", err)
+					return nil, fmt.Errorf("[decode] error: %w", err)
 				}
-				if len(remainingData) > 0 {
-					return nil, remainingData, fmt.Errorf("[decode] Remaining-data not expected (%x)", remainingData)
+				if childBuf.Len() > 0 {
+					return nil, fmt.Errorf("[decode] Remaining-data not expected (%x)", childBuf.Bytes())
 				}
-				node := NewTlvConstructedNode(tag)
-				node.children.AddNodes(*children)
+			}
 
-				nodes.AddNode(node)
-			}
+			node := NewTlvConstructedNode(tag)
+			node.children.AddNodes(*children)
+			nodes.AddNode(node)
 		} else {
-			if length == -1 { // indefinite-length
-				return nil, nil, fmt.Errorf("[decode] Indefinite-length mode is only supported for constructed tags")
-			} else {
-				value, err := utils.BytesFromBuffer(buf, int(length))
-				if err != nil {
-					return nil, nil, fmt.Errorf("[decode] ByteBuffer error: %w", err)
-				}
-				node := NewTlvSimpleNode(tag, value)
-				nodes.AddNode(node)
+			if length == -1 {
+				return nil, fmt.Errorf("[decode] Indefinite-length mode is only supported for constructed tags")
 			}
+			value, err := utils.BytesFromBuffer(buf, int(length))
+			if err != nil {
+				return nil, fmt.Errorf("[decode] ByteBuffer error: %w", err)
+			}
+			nodes.AddNode(NewTlvSimpleNode(tag, value))
 		}
 	}
 
-	return nodes, remainingData, nil
+	return nodes, nil
 }
 
 func Decode(data []byte) (nodes *TlvNodes, err error) {
-	var remainingData []byte
+	nodeCount := 0
+	buf := bytes.NewBuffer(data)
 
-	nodes, remainingData, err = decode(data)
+	nodes, err = decodeFromBuffer(buf, 0, &nodeCount)
 	if err != nil {
 		return nil, err
 	}
 
-	if len(remainingData) > 0 {
-		return nil, fmt.Errorf("[Decode] Unexpected remaining-data (%x)", remainingData)
+	if buf.Len() > 0 {
+		return nil, fmt.Errorf("[Decode] Unexpected remaining-data (%x)", buf.Bytes())
 	}
 
 	return nodes, nil
